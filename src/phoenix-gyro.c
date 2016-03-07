904415d391510095df11479f4a8ae7042c576cdb
Joe Gibson <joseph.gibson@nasa.gov>
2016-03-06 19:51:46 -0500

    Adding gyro

---
+++ src/phoenix-gyro.c
@@ -0,0 +1,171 @@
+/**********************************************************
+*   Phoenix FSW - Gyroscope
+*
+*   25 Jan 2016
+*
+*   Joe Gibson 						(joseph.gibson@nasa.gov)
+*	Jordi Vila Hernandez de Lorenzo	(jordi.vila.hdl@gmail.com)
+*
+*   License: MIT (http://gibsjose.mit-license.org)
+*
+*   https://github.com/gibsjose/phoenix
+**********************************************************/
+
+#include "phoenix-gyro.h"
+
+//Initialize gyroscope
+uint8_t gyro_init(gyro_t * gyro) {
+    uart_puts("Initializing Gyroscope");
+
+    //Allocate memory for the gyro data structure if need be
+    if(gyro == NULL) {
+        gyro = (gyro_t *) malloc(sizeof(gyro_t));
+    }
+
+    //Begin TWI communication
+    ret = i2c_start(GYRO_ADDR + I2C_WRITE);
+    if(ret) {
+        i2c_stop();
+        uart_puts("<-- ERROR: Unable to start gyroscope communication-->\r\n");
+        return ERROR;
+    }
+
+    //Set the pointer to the control 1 register
+    ret = i2c_write(GYRO_CTRL_1_REG);
+    if(ret) {
+        i2c_stop();
+        uart_puts("<-- ERROR: Could not set pointer to gyroscope CTRL 1 register -->\r\n");
+        return ERROR;
+    }
+
+    // Turn on the gyroscope and enable all axes (0x0F)
+    ret = i2c_write(GYRO_PD | GYRO_ZEN | GYRO_YEN | GYRO_XEN);
+    if(ret) {
+        i2c_stop();
+        uart_puts("<-- ERROR: Could not write to gyroscope -->\r\n");
+        return ERROR;
+    }
+
+    //Start communication again
+    ret = i2c_rep_start(GYRO_ADDR + I2C_WRITE);
+    if(ret) {
+        i2c_stop();
+        uart_puts("<-- ERROR: Unable to start gyroscope communication-->\r\n");
+        return ERROR;
+    }
+
+    //Set the pointer to the control 4 register
+    ret = i2c_write(GYRO_CTRL_4_REG);
+    if(ret) {
+        i2c_stop();
+        uart_puts("<-- ERROR: Could not set pointer to gyroscope CTRL 1 register -->\r\n");
+        return ERROR;
+    }
+
+    //Block data update: output registers not updated until read and set full scale
+    // selection to 500 dps (FS1:FS0 as 01) (0x90)
+    ret = i2c_write(GYRO_BDU | GYRO_FS0);
+    if(ret) {
+        i2c_stop();
+        uart_puts("<-- ERROR: Could not write to gyroscope -->\r\n");
+        return ERROR;
+    }
+
+    //Release bus
+    i2c_stop();
+
+    //Give the gyroscope a bit of time (250ms) to start
+    delay_us(250000);
+
+    return OK;
+}
+
+//Calibrate gyroscope
+uint8_t gyro_calibrate(gyro_t * gyro) {
+    uart_puts("Starting gyroscope calibration");
+
+    if(gyro == NULL) {
+        return ERROR;
+    }
+
+    //Sum up 2000 measurements
+    for (int i = 0; i < GYRO_CALIBRATION_STEPS; i++ ){
+        gyro_read(gyro);
+        gyro->roll_cal += gyro->roll;
+        gyro->pitch_cal += gyro->pitch;
+        gyro->yaw_cal += gyro->yaw;
+
+        //Print a '.' every 100 readings
+        if(!(i % 100)) {
+            uart_puts(".");
+        }
+
+        //Delay 4ms
+        delay_us(4000);
+    }
+}
+
+//Read pitch, roll, and yaw from gyroscope and
+// apply calibration offsets
+uint8_t gyro_read(gyro_t * gyro) {
+    unsigned char msb;
+    unsigned char lsb;
+
+    if(gyro == NULL) {
+        return ERROR;
+    }
+
+    //Point to the read register
+    i2c_start(GYRO_ADDR + I2C_WRITE);           //Begin TWI communication
+    i2c_write(GYRO_READ_REG | GYRO_AUTO_INC);   //Point to read register (0x28) and enable auto increment
+
+    //Read all six bytes
+    //@TODO Does this need to be a i2c_rep_start()
+    i2c_rep_start(GYRO_ADDR + I2C_READ);
+
+    //Roll
+    msb = i2c_readAck();
+    lsb = i2c_readAck();
+    gyro->roll = ROLL_COEFFICIENT * ((msb << 8) | lsb) - gyro->roll_offset;
+
+    //Pitch
+    msb = i2c_readAck();
+    lsb = i2c_readAck();
+    gyro->pitch = PITCH_COEFFICIENT * ((msb << 8) | lsb) - gyro->pitch_offset;
+
+    //Yaw
+    msb = i2c_readAck();
+    lsb = i2c_readNak();
+    gyro->yaw = YAW_COEFFICIENT * ((msb << 8) | lsb) - gyro->yaw_offset;
+
+    i2c_stop();
+
+    return OK;
+}
+
+//Print the gyro data
+void gyro_print(gyro_t * gyro) {
+    uart_puts("P: ");
+    uart_putd(gyro->pitch);
+    uart_puts(" --- ")
+
+    uart_puts(" R: ");
+    uart_putd(gyro->roll);
+    uart_puts(" --- ")
+
+    uart_puts(" Y: ");
+    uart_putd(gyro->yaw);
+    uart_puts(" ---\r\n");
+}
+
+//Gyro test loop
+void gyro_loop(gyro_t * gyro) {
+    //Wait 250ms
+    delay_us(250000);
+
+    //Take a reading
+    gyro_read(gyro);
+
+    //Print gyro data
+    gyro_print(gyro);
+}
---
+++ src/phoenix-gyro.h
@@ -0,0 +1,79 @@
+/**********************************************************
+*   Phoenix FSW - Gyroscope
+*
+*   25 Jan 2016
+*
+*   Joe Gibson 						(joseph.gibson@nasa.gov)
+*	Jordi Vila Hernandez de Lorenzo	(jordi.vila.hdl@gmail.com)
+*
+*   License: MIT (http://gibsjose.mit-license.org)
+*
+*   https://github.com/gibsjose/phoenix
+**********************************************************/
+
+#ifndef PHOENIX_GYRO_H
+#define PHOENIX_GYRO_H
+
+#include <avr/io.h>
+#include <string.h>
+#include "i2cmaster/i2cmaster.h"
+
+#include "phoenix-globals.h"
+#include "delay/delay.h"
+#include "uart/uart.h"
+
+//I2C Address
+#define GYRO_ADDR           0x6B    //Gyroscope I2C Address: 0x6B = 107 = 0b1101001
+
+//Configuration
+#define GYRO_CALIBRATION_STEPS  2000    ///Number of measurements during gyro calibration
+
+#define ROLL_COEFFICIENT     1.0        //Non-inverted
+#define PITCH_COEFFICIENT   -1.0        //Inverted
+#define YAW_COEFFICIENT     -1.0        //Inverted
+
+//Register Definitions
+#define GYRO_CTRL_1_REG     0x20        //@TODO Comments for these
+#define GYRO_CTRL_2_REG     0x21
+#define GYRO_CTRL_3_REG     0x22
+#define GYRO_CTRL_4_REG     0x23
+#define GYRO_CTRL_5_REG     0x24
+
+//Configuration Definitions
+////Control Register 1
+#define GYRO_PD             (1 << 3)    //Power enable
+#define GYRO_ZEN            (1 << 2)    //Z axis enable
+#define GYRO_YEN            (1 << 1)    //Y axis enable
+#define GYRO_XEN            (1 << 0)    //X axis enable
+
+////Control Register 4
+#define GYRO_BDU            (1 << 7)    //Block data update (0: contiguous update, 1: not updated until read)
+#define GYRO_FS1            (1 << 5)    //Full Scale Selection Bits (5:4)
+#define GYRO_FS0            (1 << 4)    //  00 -> 245 dps
+                                        //  01 -> 500 dps
+                                        //  1x -> 2000 dps
+
+#define GYRO_AUTO_INC       (1 << 7)    //Auto increment
+#define GYRO_READ_REG       0x28        //Read register (0x28)
+
+//Gyro data
+typedef struct gyro_t {
+    //Pitch, roll, yaw
+    double roll;            //Roll (X)
+    double pitch;           //Pitch (Y)
+    double yaw;             //Yaw (Z)
+
+    //Calibration offsets
+    double roll_offset;
+    double pitch_offset;
+    double yaw_offset;
+} gyro_t;
+
+//Function declarations
+uint8_t gyro_init(gyro_t *);
+uint8_t gyro_calibrate(gyro_t *);
+uint8_t gyro_read(gyro_t *);
+void gyro_print(gyro_t *);
+void gyro_loop(gyro_t *);
+
+#endif//PHOENIX_GYRO_H
