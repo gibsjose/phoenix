dd0766dcadfece3d840c2b2264f4529931323bf7
‘Jordi’ <jordi.vila.hdl@gmail.com>
2016-03-20 14:41:59 -0400

    scaling receiver inputs

--- src/phoenix-controls.c
+++ src/phoenix-controls.c
@@ -15,14 +15,108 @@
 
 //JVila: This should actually be on a interrput subroutine..
 void receiver_read(receiver_inputs_t * receiver){
-
+ /*receiver->roll = ...
+ receiver->pitch = ...
+ receiver->gas = ...
+ receiver->yaw = ...*/
 }
 
-//TO FINISH.
 void receiver_scale(receiver_inputs_t * receiver){
 
-  receiver->roll_scaled = receive
+//Scale ROLL
+if(receiver->roll < SCALE_CENTER_ROLL){                 //The actual receiver value is lower than the center value
+  if(receiver->roll < SCALE_MIN_ROLL){                  //The actual receiver value is lower than the minimum value
+    receiver->roll_scaled = SCALE_MIN_ROLL;             //Limit the lowest value to the value that was detected during setup
+  }
+  else{                                                 //Calculate and scale the actual value to a 1000 - 2000us value
+      double difference = ((SCALE_CENTER_ROLL - receiver->roll) * 500) / (SCALE_CENTER_ROLL - SCALE_MIN_ROLL);
+      receiver->roll_scaled = 1500 - difference * REVERSE_ROLL;
+  }
 }
+else if(receiver->roll > SCALE_CENTER_ROLL){            //The actual receiver value is higher than the center value
+  if(receiver->roll > SCALE_MAX_ROLL){                  //The actual receiver value is higher than the maximum value
+    receiver->roll_scaled = SCALE_MAX_ROLL;             //Limit the highest value to the value that was detected during setup
+  }
+  else{                                                 //Calculate and scale the actual value to a 1000 - 2000us value
+      double difference = ((receiver->roll - SCALE_CENTER_ROLL) * 500) / (SCALE_MAX_ROLL - SCALE_CENTER_ROLL);
+      receiver->roll_scaled = 1500 + difference * REVERSE_ROLL;
+  }
+}
+else{
+  receiver->roll_scaled = 1500;
+}
+
+//Scale PITCH
+if(receiver->pitch < SCALE_CENTER_PITCH){                //The actual receiver value is lower than the center value
+  if(receiver->pitch < SCALE_MIN_PITCH){                  //The actual receiver value is lower than the minimum value
+    receiver->pitch_scaled = SCALE_MIN_PITCH;             //Limit the lowest value to the value that was detected during setup
+  }
+  else{                                                 //Calculate and scale the actual value to a 1000 - 2000us value
+      double difference = ((SCALE_CENTER_PITCH - receiver->pitch) * 500) / (SCALE_CENTER_PITCH - SCALE_MIN_PITCH);
+      receiver->pitch_scaled = 1500 - difference * REVERSE_PITCH;
+  }
+}
+else if(receiver->pitch > SCALE_CENTER_PITCH){            //The actual receiver value is higher than the center value
+  if(receiver->pitch > SCALE_MAX_PITCH){                  //The actual receiver value is higher than the maximum value
+    receiver->pitch_scaled = SCALE_MAX_PITCH;             //Limit the highest value to the value that was detected during setup
+  }
+  else{                                                 //Calculate and scale the actual value to a 1000 - 2000us value
+      double difference = ((receiver->pitch - SCALE_CENTER_PITCH) * 500) / (SCALE_MAX_PITCH - SCALE_CENTER_PITCH);
+      receiver->pitch_scaled = 1500 + difference * REVERSE_PITCH;
+  }
+}
+else{
+  receiver->pitch_scaled = 1500;
+}
+
+//Scale GAS
+if(receiver->gas < SCALE_CENTER_GAS){                 //The actual receiver value is lower than the center value
+  if(receiver->gas < SCALE_MIN_GAS){                  //The actual receiver value is lower than the minimum value
+    receiver->gas_scaled = SCALE_MIN_GAS;             //Limit the lowest value to the value that was detected during setup
+  }
+  else{                                                 //Calculate and scale the actual value to a 1000 - 2000us value
+      double difference = ((SCALE_CENTER_GAS - receiver->gas) * 500) / (SCALE_CENTER_GAS - SCALE_MIN_GAS);
+      receiver->gas_scaled = 1500 - difference * REVERSE_GAS;
+  }
+}
+else if(receiver->gas > SCALE_CENTER_GAS){            //The actual receiver value is higher than the center value
+  if(receiver->gas > SCALE_MAX_GAS){                  //The actual receiver value is higher than the maximum value
+    receiver->gas_scaled = SCALE_MAX_GAS;             //Limit the highest value to the value that was detected during setup
+  }
+  else{                                                 //Calculate and scale the actual value to a 1000 - 2000us value
+      double difference = ((receiver->gas - SCALE_CENTER_GAS) * 500) / (SCALE_MAX_GAS - SCALE_CENTER_GAS);
+      receiver->gas_scaled = 1500 + difference * REVERSE_GAS;
+  }
+}
+else{
+  receiver->gas_scaled = 1500;
+}
+
+//Scale YAW
+if(receiver->yaw < SCALE_CENTER_YAW){                 //The actual receiver value is lower than the center value
+  if(receiver->yaw < SCALE_MIN_YAW){                  //The actual receiver value is lower than the minimum value
+    receiver->yaw_scaled = SCALE_MIN_YAW;             //Limit the lowest value to the value that was detected during setup
+  }
+  else{                                                 //Calculate and scale the actual value to a 1000 - 2000us value
+      double difference = ((SCALE_CENTER_YAW - receiver->yaw) * 500) / (SCALE_CENTER_YAW - SCALE_MIN_YAW);
+      receiver->yaw_scaled = 1500 - difference * REVERSE_YAW;
+  }
+}
+else if(receiver->yaw > SCALE_CENTER_YAW){            //The actual receiver value is higher than the center value
+  if(receiver->yaw > SCALE_MAX_YAW){                  //The actual receiver value is higher than the maximum value
+    receiver->yaw_scaled = SCALE_MAX_YAW;             //Limit the highest value to the value that was detected during setup
+  }
+  else{                                                 //Calculate and scale the actual value to a 1000 - 2000us value
+      double difference = ((receiver->yaw - SCALE_CENTER_YAW) * 500) / (SCALE_MAX_YAW - SCALE_CENTER_YAW);
+      receiver->yaw_scaled = 1500 + difference * REVERSE_YAW;
+  }
+}
+else{
+  receiver->yaw_scaled = 1500;
+}
+
+}
+
 
 
 void receiver_read(gyro_t * gyro) {
--- src/phoenix-controls.h
+++ src/phoenix-controls.h
@@ -24,55 +24,25 @@
 #include "delay/delay.h"
 #include "uart/uart.h"
 
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //PID gain and limit settings
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Gains for the ROLL PID, are the same as the PITCH PID
+#define P_ROLL = 1.4;
+#define I_ROLL = 0.05;
+#define D_ROLL = 15;
 
-//PID gains and settings
-typedef struct PID_Settings_t {
-    //Coeficients
-    double P;           // Proportional gain
-    double I;           // Integral gain
-    double D;           // Derivative gain
+#define P_YAW = 4;
+#define I_YAW = 0.02;
+#define D_YAW = 0;
 
-    //Limits of the contribution of each PID controller
-    double upper_limit;
-    double lower_limit;
-} PID_Settings_t;
+#define UPPER_LIMIT = 400;
+#define LOWER_LIMIT = -400;
 
-PID_Settings_t pid_settings_roll = {
-    1.4,               // Gain setting for the roll P-controller
-    0.05,              // Gain setting for the roll I-controller
-    15,                // Gain setting for the roll D-controller
-    400,               // Maximum output of the PID-controller
-    -400,              // Minimum output of the PID-controller
-};
 
-PID_Settings_t pid_settings_pitch = pid_settings_roll ;
-
-PID_Settings_t pid_settings_yaw = {
-    4,                 // Gain setting for the yaw P-controller
-    0.02,              // Gain setting for the yaw I-controller
-    0,                 // Gain setting for the yaw D-controller
-    400,               // Maximum output of the PID-controller
-    -400,              // Minimum output of the PID-controller
-};
-
-//Remote controller receiver inputs for the Futaba 7C transmitter and Futaba R617FS receiver.
-typedef struct receiver_t {
-    // roll, pitch, gas and yaw receiver inputs.
-    double roll;            // Roll channel 1
-    double pitch;           // Pitch channel 2
-    double gas;             // Throttle channel 3
-    double yaw;             // Yaw channel 4
-
-    // The raw inputs will not exactly be on the [1000,2000] microsecond range. The scaled values will be on that range
-    double roll_scaled;            // Roll channel 1
-    double pitch_scaled;           // Pitch channel 2
-    double gas_scaled;             // Throttle channel 3
-    double yaw_scaled;             // Yaw channel 4
-} receiver_t;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Radio receivers details
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //These values are used to fit the reciever inputs into a range from 1000 to 2000.
 //The pilot should set these values on a startup calibration procedure
@@ -92,66 +62,69 @@ typedef struct receiver_t {
 #define SCALE_MAX_YAW = 2000;
 #define SCALE_MIN_YAW = 1000;
 
+// Some remotes have the receiver inputs are reversed
+#define REVERSE_ROLL = 1; //1 channel not reversed, -1 channel reversed
+#define REVERSE_PITCH = 1;
+#define REVERSE_GAS = 1;
+#define REVERSE_YAW = 1;
 
-//Question: What does return OK do when the expected return is a uint8_t ?
+
+//PID gains and settings data
+typedef struct PID_Settings_t {
+    //Coeficients
+    double P;           // Proportional gain
+    double I;           // Integral gain
+    double D;           // Derivative gain
+
+    //Limits of the contribution of each PID controller
+    double upper_limit;
+    double lower_limit;
+} PID_Settings_t;
+
+PID_Settings_t pid_settings_roll = {
+    P_ROLL,               // Gain setting for the roll P-controller
+    I_ROLL,              // Gain setting for the roll I-controller
+    D_ROLL,                // Gain setting for the roll D-controller
+    UPPER_LIMIT,               // Maximum output of the PID-controller
+    LOWER_LIMIT,              // Minimum output of the PID-controller
+};
+
+PID_Settings_t pid_settings_pitch = pid_settings_roll ;
+
+PID_Settings_t pid_settings_yaw = {
+    P_YAW,                 // Gain setting for the yaw P-controller
+    I_YAW,              // Gain setting for the yaw I-controller
+    D_YAW,                 // Gain setting for the yaw D-controller
+    UPPER_LIMIT,               // Maximum output of the PID-controller
+    LOWER_LIMIT,              // Minimum output of the PID-controller
+};
+
+//Remote controller receiver inputs for the Futaba 7C transmitter and Futaba R617FS receiver.
+typedef struct receiver_t {
+    // roll, pitch, gas and yaw receiver inputs.
+    double roll;            // Roll channel 1
+    double pitch;           // Pitch channel 2
+    double gas;             // Throttle channel 3
+    double yaw;             // Yaw channel 4
+
+    // The raw inputs will not exactly be on the [1000,2000] microsecond range. The scaled values will be on that range
+    double roll_scaled;            // Roll channel 1
+    double pitch_scaled;           // Pitch channel 2
+    double gas_scaled;             // Throttle channel 3
+    double yaw_scaled;             // Yaw channel 4
+} receiver_t;
+
+
+
+
+//Function declarations
 void receiver_read(receiver_t *);
 void receiver_scale(receiver_t *);
 
 
-
-
-//I2C Address
-#define GYRO_ADDR           0xD6    //Gyroscope I2C Address: 0xD6 = 214 = 0b11010010
-
-//Configuration
-#define GYRO_CALIBRATION_STEPS  200    ///Number of measurements during gyro calibration
-
-#define ROLL_COEFFICIENT     1.0        //Non-inverted
-#define PITCH_COEFFICIENT   -1.0        //Inverted
-#define YAW_COEFFICIENT     -1.0        //Inverted
-
-//Register Definitions
-#define GYRO_CTRL_1_REG     0x20        //@TODO Comments for these
-#define GYRO_CTRL_2_REG     0x21
-#define GYRO_CTRL_3_REG     0x22
-#define GYRO_CTRL_4_REG     0x23
-#define GYRO_CTRL_5_REG     0x24
-
-//Configuration Definitions
-////Control Register 1
-#define GYRO_PD             (1 << 3)    //Power enable
-#define GYRO_ZEN            (1 << 2)    //Z axis enable
-#define GYRO_YEN            (1 << 1)    //Y axis enable
-#define GYRO_XEN            (1 << 0)    //X axis enable
-
-////Control Register 4
-#define GYRO_BDU            (1 << 7)    //Block data update (0: contiguous update, 1: not updated until read)
-#define GYRO_FS1            (1 << 5)    //Full Scale Selection Bits (5:4)
-#define GYRO_FS0            (1 << 4)    //  00 -> 245 dps
-                                        //  01 -> 500 dps
-                                        //  1x -> 2000 dps
-
-#define GYRO_AUTO_INC       (1 << 7)    //Auto increment
-#define GYRO_READ_REG       0x28        //Read register (0x28)
-
-//Gyro data
-typedef struct gyro_t {
-    //Pitch, roll, yaw
-    double roll;            //Roll (X)
-    double pitch;           //Pitch (Y)
-    double yaw;             //Yaw (Z)
-
-    //Calibration offsets
-    double roll_offset;
-    double pitch_offset;
-    double yaw_offset;
-} gyro_t;
-
-//Function declarations
+//Question: What does return OK do when the expected return is a uint8_t ?
+/*//Function declarations
 uint8_t gyro_init(gyro_t *);
-uint8_t gyro_calibrate(gyro_t *);
-uint8_t gyro_read(gyro_t *);
-void gyro_print(gyro_t *);
-void gyro_loop(gyro_t *);
+*/
 
-#endif//PHOENIX_GYRO_H
+#endif//PHOENIX_CONTROLS_H
